c24c8b307e7697989815d75bf53d679bc2ea588d
seb <contact@smaloron.eu>
2024-10-12 19:00:06 +0200

    lazy loading

---
+++ src/productDetails.js
@@ -0,0 +1,7 @@
+export default function loadProductDetails() {
+    const productDetailsDiv = document.getElementById('productDetails');
+    productDetailsDiv.innerHTML = `
+    <h2>Product Details</h2>
+    <p>Here are some details about the product...</p>
+  `;
+}
